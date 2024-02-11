12512bf64d20191e3fe073772be47b1ce6170272
kaisa.l <kaisa.oyre.larsen@gmail.com>
2024-02-11 21:03:26 +0100

    feat: add flashcard component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./styles/App.css";
+import "./components/Flashcard.jsx"
+import Flashcard from "./components/Flashcard.jsx";
 
 function App() {
 
   return (
-    <div>
-      Hello world :)
+    <div className="App">
+        <Flashcard term={"Hvordan slayer man best mulig?"} definition={"Bånner en sekser og en flaske jäger"} />
     </div>
   );
 }
