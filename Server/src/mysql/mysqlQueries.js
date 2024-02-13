61ca1cedd2fa6a161f4ea8302ca24b7508fde717
Klokk <sanderklo@live.no>
2024-02-13 12:57:23 +0100

    \#11 la til SQL queries @fredbaks

--- Server/src/mysql/mysqlQueries.js
+++ Server/src/mysql/mysqlQueries.js
@@ -13,11 +13,43 @@ export const getCountry = async (id) => {
 };
 */
 
-export const getBruker = async () => {
-  
-  const [rows] = (await pool
-    .promise()
-    .query('SELECT * FROM Bruker'));
-  return rows[0];
-};
+/*let IDs = ['1,2,3,4,5,6,7']*/
 
+export const getBruker = async (IDs) => {
+  const [rows] = (await pool
+        .promise()
+        .query("SELECT * FROM Bruker WHERE ID in (?)", [IDs], function(err, result) {
+        if(err) {
+          console.log(err);
+          } else {
+            console.log(result);
+        }
+      }))
+      return rows;
+}
+
+export const getFlashcardSet = async (IDs) => {
+  const [rows] = (await pool
+        .promise()
+        .query("SELECT * FROM FlashcardSet WHERE ID in (?)", [IDs], function(err, result) {
+        if(err) {
+          console.log(err);
+          } else {
+          console.log(result);
+        }
+      }))
+      return rows;
+}
+
+export const getKommnetar = async (IDs) => {
+  const [rows] = (await pool
+        .promise()
+        .query("SELECT * FROM Kommentar WHERE ID in (?)", [IDs], function(err, result) {
+        if(err) {
+          console.log(err);
+          } else {
+            console.log(result);
+        }
+      }))
+      return rows;
+}
