b654fd04fa84b1c9924652b0b8e5b095c50b5165
Klokk <sanderklo@live.no>
2024-02-15 13:07:24 +0100

    \#11 La til flere queries fikeset skrivefeil @fredbaks

--- Server/src/mysql/mysqlQueries.js
+++ Server/src/mysql/mysqlQueries.js
@@ -22,7 +22,7 @@ export const getBruker = async (IDs) => {
         if(err) {
           console.log(err);
           } else {
-            console.log(result);
+          console.log(result);
         }
       }))
       return rows;
@@ -48,8 +48,149 @@ export const getKommnetar = async (IDs) => {
         if(err) {
           console.log(err);
           } else {
-            console.log(result);
+          console.log(result);
         }
       }))
       return rows;
+}
+
+export const getFavoritSet = async (IDs) => {
+  const [rows] = (await pool
+        .promise()
+        .query("SELECT * FROM FavorittSet WHERE BrukerID in (?)", [IDs], function(err, result) {
+        if(err) {
+          console.log(err);
+          } else {
+          console.log(result);
+        }
+      }))
+      return rows;
+}
+
+export const getPopulereSet = async ()=> {
+  const [rows] = (await pool
+    .promise()
+    .query("SELECT * FROM FlashcardSet ORDER BY Likes",)
+    )
+    return rows;
+}
+
+
+
+export const addBruker = async (Data) => {
+  await pool
+  .promise()
+  .query("INSERT INTO Bruker (?)", [Data], function(err) {
+    if(err) {
+      console.log(err);
+      }
+    }
+  )
+}
+
+export const addLikeSet = async (ID) => {
+  await pool
+  .promise()
+  .query("UPDATE FlashcardSet Set Likes = Likes + 1 WHERE ID = (?)" , [ID], function(err){
+    if(err) {
+      console.log(err);
+    }
+  })
+}
+
+export const addLikeBruker = async (IDs) => {
+  await pool
+  .promise()
+  .query("INSERT INTO FavorittSet (?)", [IDs])
+}
+
+export const addFlashcardSet = async (Data) => {
+  await pool
+  .promise()
+  .query("INSERT INTO FlashcardSet (?)", [Data], function(err) {
+    if(err) {
+      console.log(err);
+      }
+    }
+  )
+}
+
+export const addKommnetar = async (Data) => {
+  await pool
+  .promise()
+  .query("INSERT INTO Kommnetar (?)", [Data], function(err) {
+    if(err) {
+      console.log(err);
+      }
+    }
+  )
+}
+
+export const removeFavorittSet = async (ID) => {
+  await pool
+  .promise()
+  .query("DELETE FROM FavorittSet WHERE SetID = (?)", [ID], function(err) {
+    if(err) {
+      console.log(err);
+      }
+  })
+}
+
+export const removeBruker = async (ID) => {
+  await pool
+  .promise()
+  const [data] = pool.query("SELECT * Favorittsett WHERE BrukerID = (?)", [ID], function(err,result){
+    if(err) {
+      console.log(err);
+      } else {
+      console.log(result);
+    }
+  })
+  .query("DELETE FROM Bruker WHERE ID = (?)", [ID], function(err){
+    if(err) {
+      console.log(err);
+      }
+  })
+  .query("DELETE FROM FavorittSet WHERE BrukerID = (?)", [ID], function(err){
+    if(err) {
+      console.log(err);
+      }
+  })
+  .query("UPDATE FlashcardSet Set Likes = Likes - 1 WHERE ID = (?)" , [data], function(err){
+    if(err) {
+      console.log(err);
+    }
+  })
+}
+
+export const removeKommentar = async (ID) => {
+  await pool
+  .promise()
+  .query("DELETE FROM Kommentar WHERE ID = (?)", [ID], function(err) {
+    if(err) {
+      console.log(err);
+      }
+  })
+}
+
+export const updateFlashcardSet = async (ID,data) => {
+  let query = "UPDATE FlashcardSet Set Data = " + data + " Where ID = (?)"
+  await pool
+  .promise()
+  .query(query, [ID], function(err) {
+    if(err) {
+      console.log(err);
+      }
+  })
+}
+
+export const updateAdmin = async (ID,data) => {
+  let query = "UPDATE Bruker Set Admin = " + data + " Where ID = (?)"
+  await pool
+  .promise()
+  .query(query, [ID], function(err) {
+    if(err) {
+      console.log(err);
+      }
+  })
 }
