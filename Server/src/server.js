240832fbfd7eb61f80551338fda12c4308475956
Fredrik Baksaas <fredbaks@stud.ntnu.no>
2024-02-15 13:53:20 +0100

    #11 koblet opp server og client

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,9 +1,32 @@
-import { getBruker } from "./mysql/mysqlQueries.js";
+/**
+import express from "express";
+import cors from "cors";
+import { getAllBruker } from "./mysql/mysqlQueries.js";
 
-const bruker = getBruker();
-
-console.log(bruker);
+const brukere = getAllBruker();
 
 setInterval(() => {
-    console.log(bruker);
-}, 2000);
+  console.log(brukere);
+}, 1000);
+*/
+import express from "express";
+import { getBruker } from "./mysql/mysqlQueries.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.listen(3000);
+
+app.get("/bruker", async (req, res) => {
+  const brukerID = req.query.ID;
+  console.log(brukerID);
+  const bruker = await getBruker(brukerID);
+  console.log(bruker);
+  res.send(bruker);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
